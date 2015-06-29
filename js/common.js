540a553360c85b54902aac4034e4f18807f0f78a
hmelnov <pavelhmelnov@gmail.com>
2015-06-29 15:48:43 +0500

    first commit

---
+++ js/common.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+	
+	$(".top_mnu ul a[href*='#']").mPageScroll2id();
+
+	$(".popup").magnificPopup({type:"image"});
+	$(".popup_content").magnificPopup({type:"inline", closeBtnInside: true, showCloseBtn: true});
+
+
+	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
+	$(".top_text p, .section_header").animated("fadeInUp", "fadeOutDown");
+	$(".animation_1").animated("flipInY", "flipOutY");
+	$(".animation_2").animated("fadeInLeft", "fadeOutRight");
+	$(".animation_3").animated("fadeInRight", "fadeOutLeft");
+	$(".left .resume_item").animated("fadeInLeft", "fadeOutRight");
+	$(".right .resume_item").animated("fadeInRight", "fadeOutLeft");
+	$(".s_content").animated("fadeIn","fadeOut");
+
+	function heightDetect() {
+	$(".main_head").css("height", $(window).height());
+	}
+	heightDetect();
+	$(window).resize(function () {
+		heightDetect();
+	});
+
+	$(".toggle_mnu, .menu_item").click(function () {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".top_mnu li a").click(function () {
+		$(".top_mnu").fadeOut(600);
+		$(".top_mnu li a").removeClass("fadeInUp animated");
+		$(".sandwich").toggleClass("active");
+		$(".toggle_mnu").removeClass("active");
+		$(".top_text").removeClass("h_opacity");
+	});
+
+	$(".toggle_mnu").click(function () {
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_text").removeClass("h_opacity");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+			$(".toggle_mnu").removeClass("active");
+		} else {
+		$(".top_text").addClass("h_opacity");
+		$(".top_mnu").fadeIn(600);
+		$(".top_mnu li a").addClass("fadeInUp animated");
+		$(".toggle_mnu").addClass("active");
+		}
+	});
+
+	$("#portfolio_grid").mixItUp();
+
+	$(".s_portfolio li").click(function () {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	$(".portfolio_item").each(function(i) {
+		$(this).find("a").attr("href", "#work_" + i);
+		$(this).find(".port_descr").attr("id", "work_" + i);
+	});
+
+	$(function () { $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(); } );
+	
+});
+$(window).load(function() {
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+});
